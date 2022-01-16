e38aca357ef0a9ffa3c6f9db20e1e4d4cac801ce
Tenzin <tenthinten@gmail.com>
2022-01-16 22:07:23 +0530

    install send button

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -23,6 +23,10 @@ const MessageForm = (props) => {
         isTyping(props, chatId);
     }
 
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, {files:event.target.value, text:''})
+    }
+
     return (
         <form className="message-form" onSubmit = {handleSubmit}>
             <input 
@@ -37,6 +41,16 @@ const MessageForm = (props) => {
                     <PictureOutlined  className="picture-icon"/>
                </span>
            </label>
+           <input 
+            type="file"
+            multiple={false}
+            id="uplaod-button"
+            style={{display: 'none'}}
+            onChange={handleUpload}
+           />
+           <button type="submit" className="send-button">
+               <SendOutlined className="send-icon" />
+           </button>
         </form>
     ) 
 
