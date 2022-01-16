55b64d699ea44f77b1c1c62f0b8ba8a0c3dd6ffe
Tenzin <tenthinten@gmail.com>
2022-01-16 14:02:13 +0530

    return image

--- src/components/TheirMessage.js
+++ src/components/TheirMessage.js
@@ -9,6 +9,22 @@ const TheirMessage = ({lastMessage, message}) => {
                     style={{backgroundImage: `url(${message?.sender?.avater})`}}
                 />
             )}
+            
+            {message?.attachments?.length>0
+                    ? (
+                    <img
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{float:'right'}}
+                    />
+                    ):
+                    (
+                        <div className="message" style={{float:'right', marginRight: '18px', color:'white', backgroundColor:'#3B2A50'}}>
+                            {message.text}
+                        </div>
+                    )      
+            }
         </div>
     ) 
 
