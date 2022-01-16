a582c2d540be6869f0d8df5d8b4148b5ed6b31da
Tenzin <tenthinten@gmail.com>
2022-01-16 13:40:02 +0530

    add avatar

--- src/components/TheirMessage.js
+++ src/components/TheirMessage.js
@@ -2,8 +2,13 @@ const TheirMessage = ({lastMessage, message}) => {
     const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
 
     return (
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avater"
+                    style={{backgroundImage: `url(${message?.sender?.avater})`}}
+                />
+            )}
         </div>
     ) 
 
